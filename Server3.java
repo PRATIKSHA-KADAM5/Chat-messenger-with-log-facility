504b9b4bde82f726d0fff841de58a93a0d223308
PRATIKSHA-KADAM5 <125458855+PRATIKSHA-KADAM5@users.noreply.github.com>
2023-02-18 20:37:54 +0530

    Update Server3.java

--- Server3.java
+++ Server3.java
@@ -8,16 +8,21 @@ public class Server3
         System.out.println("Server application is running...");
 
         ServerSocket ss = new ServerSocket(2100);
+        //It is object of server socket class which is used to create the server & start the server in leasing mode
         System.out.println("Server is running at port no 2100 and waiting for client request");
 
         Socket s = ss.accept();
+        //It is obejct of socket class which is used to accept the request which come from the client.
         System.out.println("Request of client gets accepted");
 
         PrintStream ps = new PrintStream(s.getOutputStream());
+        //It is object of printstream class used to send the data to client application.
 
         BufferedReader br1 = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+        //It is onject of bufferd reader used to accept the input from client.
         
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+        //It is a object of buffred reader used to accpet the input from user.
         String str1, str2;
 
         while((str1 = br1.readLine()) != null)
